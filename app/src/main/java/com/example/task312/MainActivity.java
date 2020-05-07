2545aba6e7e1891e307815777e4b4dd7adb90f2b
Maxim Maxim <xam2188@list.ru>
2020-05-07 23:14:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/task312/MainActivity.java
@@ -0,0 +1,221 @@
+package com.example.task312;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean add, sub, mul, div, percnt;
+    float res1, res2;
+
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView screen = findViewById(R.id.textViewNumb);
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+        if(getIntent().hasExtra("byteArray")) {
+            ImageView previewThumbnail = new ImageView(this);
+            Bitmap b = BitmapFactory.decodeByteArray(
+                    getIntent().getByteArrayExtra("byteArray"),0,getIntent().getByteArrayExtra("byteArray").length);
+            previewThumbnail.setImageBitmap(b);
+        }
+        Button n0 = findViewById(R.id.btn_zero);
+        Button n1 = findViewById(R.id.btn_one);
+        Button n2 = findViewById(R.id.btn_two);
+        Button n3 = findViewById(R.id.btn_three);
+        Button n4 = findViewById(R.id.btn_four);
+        Button n5 = findViewById(R.id.btn_five);
+        Button n6 = findViewById(R.id.btn_six);
+        Button n7 = findViewById(R.id.btn_seven);
+        Button n8 = findViewById(R.id.btn_eight);
+        Button n9 = findViewById(R.id.btn_nine);
+        Button dot = findViewById(R.id.btn_dot);
+        Button equals = findViewById(R.id.btn_equally);
+        Button addit = findViewById(R.id.btnPlus);
+        Button minus = findViewById(R.id.btnMinus);
+        Button multip = findViewById(R.id.btnMultip);
+        Button division = findViewById(R.id.btn_division);
+        Button plusMinus = findViewById(R.id.btn_plus_minus);
+        final Button percent = findViewById(R.id.btn_persent);
+        Button clear = findViewById(R.id.btn_reset);
+
+        final View.OnClickListener calculatorListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = v.getId();
+                switch (id) {
+                    case R.id.btn_zero:
+                        screen.append("0");
+                        break;
+                    case R.id.btn_one:
+                        screen.append("1");
+                        break;
+                    case R.id.btn_two:
+                        screen.append("2");
+                        break;
+                    case R.id.btn_three:
+                        screen.append("3");
+                        break;
+                    case R.id.btn_four:
+                        screen.append("4");
+                        break;
+                    case R.id.btn_five:
+                        screen.append("5");
+                        break;
+                    case R.id.btn_six:
+                        screen.append("6");
+                        break;
+                    case R.id.btn_seven:
+                        screen.append("7");
+                        break;
+                    case R.id.btn_eight:
+                        screen.append("8");
+                        break;
+                    case R.id.btn_nine:
+                        screen.append("9");
+                        break;
+                    case R.id.btn_dot:
+                        screen.append(".");
+                        break;
+                    case R.id.btnPlus:
+                        if (screen == null) {
+                            res1 = Float.parseFloat(screen.getText() + "");
+                            add = true;
+                            screen.setText(null);
+                        }
+                        break;
+                    case R.id.btnMinus:
+                        if (screen == null) {
+                            res1 = Float.parseFloat(screen.getText() + "");
+                            sub = true;
+                            screen.setText(null);
+                        }
+                        break;
+                    case R.id.btn_equally:
+                        res2 = Float.parseFloat(screen.getText() + "");
+                        if (add == true) {
+                            screen.setText(res1 + res2 + "");
+                            add = false;
+                        }
+                        if (sub == true) {
+                            screen.setText(res1 - res2 + "");
+                            sub = false;
+                        }
+                        if (mul == true) {
+                            screen.setText(res1 * res2 + "");
+                            mul = false;
+                        }
+                        if (div == true) {
+                            screen.setText(res1 / res2 + "");
+                            div = false;
+                        }
+                        if (percnt == true) {
+                            screen.setText((res1 * res2) / 100 + "");
+                            percnt = false;
+                        }
+                        break;
+                    case R.id.btnMultip:
+                        if (screen == null) {
+                            res1 = Float.parseFloat(screen.getText() + "");
+                            mul = true;
+                            screen.setText(null);
+                        }
+                        break;
+                    case R.id.btn_division:
+                        if (screen == null) {
+                            res1 = Float.parseFloat(screen.getText() + "");
+                            div = true;
+                            screen.setText(null);
+                        }
+                        break;
+                    case R.id.btn_plus_minus:
+                        String displayElements = screen.getText().toString();
+                        int length = displayElements.length();
+                        if (length > 0) {
+                            displayElements = displayElements.substring(0, length - 1);
+                            screen.setText(displayElements);
+                        }
+                        break;
+                    case R.id.btn_persent:
+                        if (screen == null) {
+                            res1 = Float.parseFloat(screen.getText() + "");
+                            percnt = true;
+                            screen.setText(null);
+                        }
+                        break;
+                    case R.id.btn_reset:
+                        screen.setText("");
+                        break;
+
+                }
+            }
+        };
+
+        n0.setOnClickListener(calculatorListener);
+        n1.setOnClickListener(calculatorListener);
+        n2.setOnClickListener(calculatorListener);
+        n3.setOnClickListener(calculatorListener);
+        n4.setOnClickListener(calculatorListener);
+        n5.setOnClickListener(calculatorListener);
+        n6.setOnClickListener(calculatorListener);
+        n7.setOnClickListener(calculatorListener);
+        n8.setOnClickListener(calculatorListener);
+        n9.setOnClickListener(calculatorListener);
+        dot.setOnClickListener(calculatorListener);
+        equals.setOnClickListener(calculatorListener);
+        addit.setOnClickListener(calculatorListener);
+        minus.setOnClickListener(calculatorListener);
+        multip.setOnClickListener(calculatorListener);
+        division.setOnClickListener(calculatorListener);
+        plusMinus.setOnClickListener(calculatorListener);
+        percent.setOnClickListener(calculatorListener);
+        clear.setOnClickListener(calculatorListener);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent intentNotes = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intentNotes);
+            Toast.makeText(MainActivity.this, R.string.setActivity, Toast.LENGTH_LONG).show();
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/task312/SettingsActivity.java
@@ -0,0 +1,121 @@
+package com.example.task312;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_PERMISSION_READ_STORAGE = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Button btn = findViewById(R.id.butOk);
+
+        final int permissionStatus = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+
+                    LoadImg();
+                } else {
+                    ActivityCompat.requestPermissions(SettingsActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_CODE_PERMISSION_READ_STORAGE);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_PERMISSION_READ_STORAGE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    LoadImg();
+                } else {
+                }
+                return;
+        }
+    }
+
+    private void LoadImg() {
+
+        ImageView viewSet = findViewById(R.id.imageViewSet);
+        if (isExternalStorageWritable()) {
+            File logFile = new File(getApplicationContext().getExternalFilesDir(null), "log.txt");
+            try {
+                FileWriter logWriter = new FileWriter(logFile);
+                logWriter.append("App loaded");
+                logWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            final EditText editText = findViewById(R.id.editTextSet);
+            String strText = editText.getText().toString();
+            int intPic = Integer.parseInt(strText);
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                    String.valueOf(intPic) + ".jpg");
+            Bitmap b = BitmapFactory.decodeFile(file.getAbsolutePath());
+
+            Intent i = new Intent(this, MainActivity.class);
+            ByteArrayOutputStream bs = new ByteArrayOutputStream();
+            b.compress(Bitmap.CompressFormat.PNG, 50, bs);
+            i.putExtra("byteArray", bs.toByteArray());
+            startActivity(i);
+            
+//            view.buildDrawingCache();
+//            b = view.getDrawingCache();
+            viewSet.setImageBitmap(b);
+//            intentNotes = new Intent(SettingsActivity.this, MainActivity.class);
+//            intentNotes.putExtra("BitImg", b);
+//            startActivity(intentNotes);
+            Toast.makeText(this, file.getAbsolutePath(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
+
+
